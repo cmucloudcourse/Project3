5cea950515c90230a3d8367784d1c59640565667
Manas Shukla <mshukla@apple.com>
2018-06-28 15:37:20 -0700

    Commiting task3 ui

--- task3/playground_ui/playground.py
+++ task3/playground_ui/playground.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import requests,json
 
-pythonServiceHostName = "http://<ip>";
+pythonServiceHostName = "http://playground-task3-service.task3.svc.cluster.local:6000";
 
 app = Flask(__name__, static_folder='site', static_url_path='')
 
@@ -12,22 +12,36 @@ def handle():
 @app.route("/python", methods=['GET', 'POST'])
 def handlePython():
     if request.method == 'POST':
-        code = request.form['code']
         payload = {}
-        payload['code']= code
-        r = requests.post("http://localhost:6000/py/eval", json.dumps(payload))
-        # print r.text
-        print "This is the result json"
-        print r.json()
+        if request.json :
+            print "Request contains JSON data"
+            code = request.get_json(force=True)
+            print code
+            payload=code
+        else :
+            print "Request contains form data"
+            code = request.form['code']
+            payload['code'] = code
+
+        print "Converted Payload JSON is "+json.dumps(payload)
+        res = fwdreq(json.dumps(payload))
         # This should return the stdout and stderr in json format
         # return the exact response fom pyService.py only!
         # Your code should handle 'code' as an argument in both
         # request.form and request.json
         ### BEGIN STUDENT CODE ###
         ### END STUDENT CODE ###
-        return r.text
+
+        return res
     else:
         return app.send_static_file("python.html")
+
+def fwdreq(command):
+    r = requests.post(pythonServiceHostName+"/py/eval", command)
+    # print r.text
+    print "Results ::::: This is the result json"
+    print r.json()
+    return r.text
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
